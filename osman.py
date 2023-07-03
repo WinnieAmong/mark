a347e4c1808284a22ac8e8ce1e35b7cf7dad53e5
Esther Auma <estheraumaego@gmail.com>
2023-07-03 14:18:16 +0300

    adda the file called osman.py

---
+++ osman.py
@@ -0,0 +1,24 @@
+
+def my_name (fname):
+    print (fname  +  " Bless")
+
+my_name ("Esther")
+my_name ("Baby")
+
+def data(fname, lname):
+    print(fname + lname)
+     
+data("esther ", "auma")
+data("monica ","ajwang")
+
+
+def my_function(*kids):
+    print("The youngest child is " +  kids[2])
+
+my_function("Patricia","Elizabeth","Rabecca")
+
+def my_statement(*list):
+    print("I hate being " + list[0])
+
+my_statement("lazy","angry")
+
